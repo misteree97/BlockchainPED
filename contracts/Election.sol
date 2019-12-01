5490398a358a35af876e8d94a4c156cc28eecaaf
Elliot Lohr <elliot-lohr@uiowa.edu>
2019-11-30 18:47:18 -0600

    voting implemented

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -9,9 +9,10 @@ contract Election {
     }
     string public candidate;
 
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
     // Read/write candidates
     mapping(uint => Candidate) public candidates;
-
     // Store Candidates Count
     uint public candidatesCount;
 
@@ -23,5 +24,18 @@ contract Election {
 	function addCandidate (string memory _name) private {
 		candidatesCount++;
 		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+	function vote (uint _candidateId) public {
+		//require that they haven't voted before
+		require(!voters[msg.sender]);
+
+		//require a valid candidate
+		require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+		//record that the voter has voted
+		voters[msg.sender] = true;
+
+		//update candidate vote count
+		candidates[_candidateId].voteCount ++;
 	}
 }
